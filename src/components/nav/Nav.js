358b871aefe78485e8494d118c255eaa14c64d10
rwilliams01101 <rwilliams01101@gmail.com>
2021-08-02 16:56:47 +0200

    adjusted logo

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -6,7 +6,7 @@ function Nav () {
         <>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container-fluid">
-                    <a className="navbar-brand navbar-main-font" href="/"> 🌲lan & Camille</a>
+                    <a className="navbar-brand navbar-main-font" href="/"> <span className="tighten-logo">🌲</span>lan & Camille</a>
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     </ul>
                     <span className="navbar-text">
