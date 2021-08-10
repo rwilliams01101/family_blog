bb8417636fb9918b59c19b7f6f97f3703226a930
rwilliams01101 <rwilliams01101@gmail.com>
2021-08-10 12:11:59 +0200

    changed a element to Link react-router-dom element

--- src/routes/Blogpost.js
+++ src/routes/Blogpost.js
@@ -2,6 +2,7 @@ import Image from '../components/image/Image'
 import Text from '../components/text/Text'
 import { Blogpost_Values } from '../utils/AppConstants'
 
+
 function Blogpost() {
 
     const value = localStorage.getItem('blogPostNumber');
@@ -15,15 +16,14 @@ function Blogpost() {
 
     return (
         <>
-        <h1>Something here</h1>
-             {/* {blogEntries.map((value, i) => (
+             {blogEntries.map((value, i) => (
                 <div key={blogEntries[i].id}>
                     <Image src={blogEntries[i].src} classes={blogEntries[i].classes} altText={blogEntries[i].altText}></Image>
                     <Text classes="tight-caption" content={blogEntries[i].altText}></Text>
                     <Text classes="blog-entry" content={blogEntries[i].blogEntry}></Text>
                     <hr />
                 </div>
-            ))} */}
+            ))}
         </>
     )
   }
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Image from '../components/image/Image'
 import Text from '../components/text/Text'
+import { Link } from "react-router-dom";
 import { Preview_Values } from '../utils/AppConstants'
 import StoreLocally from '../utils/StoreLocally'
 
@@ -13,7 +14,7 @@ function Home(props) {
                     <Image src={Preview_Values[i].src} classes={Preview_Values[i].classes} altText={Preview_Values[i].altText}></Image>
                     <Text classes="tight-caption" content={Preview_Values[i].altText}></Text>
                     <Text classes="blog-entry" content={Preview_Values[i].blogEntry}></Text>
-                    <a className="read-more" href={"/blogpost"} value={Preview_Values[i].id} onClick={()=>StoreLocally(value.id)}>Read full post</a>
+                    <Link className="read-more" to={"/blogpost"} value={Preview_Values[i].id} onClick={()=>StoreLocally(value.id)}>Read full post</Link>
                     <hr />
                 </div>
             ))}
